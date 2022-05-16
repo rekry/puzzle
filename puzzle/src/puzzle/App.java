c411c86cd408936d359f0873de0d06cd62f95e9d
rekry <53280529+rekry@users.noreply.github.com>
2022-05-16 23:19:55 +0200

    Update App.java

--- puzzle/src/puzzle/App.java
+++ puzzle/src/puzzle/App.java
@@ -3,7 +3,5 @@ package puzzle;
 public class App {
     public static void main(String[] args) {
         new GameFrame().buildFrame();
-        //multiple args of the same value (int...value) its an array
-        //and when calling the function (1,2,3, ect)
     }
 }
